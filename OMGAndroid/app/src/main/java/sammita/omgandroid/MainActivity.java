1d89121dee52b4c68f88814e7112f9649abf0efc
sammita94 <sammitachakravarti@yahoo.com>
2016-01-19 23:21:06 +0530

    changes

--- OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
+++ OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    TextView mainTextView;
+    Button mainButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
     }
 
     @Override
@@ -21,4 +31,11 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        mainTextView.setText("Button pressed!");
+
+
+    }
 }

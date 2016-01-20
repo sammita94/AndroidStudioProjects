67059d97a0736d8f8f139a618c4df8ec93c80910
sammita94 <sammitachakravarti@yahoo.com>
2016-01-21 01:56:20 +0530

    -

--- OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
+++ OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
@@ -3,15 +3,25 @@ package sammita.omgandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
+
+    ListView mainListView;
+    ArrayAdapter<String> mArrayAdapter;
+    ArrayList<String> mNameList = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+// Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        mainListView.setAdapter(mArrayAdapter);
+
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+// Set the ListView to use the ArrayAdapter
+
     }
 
     @Override
@@ -34,7 +62,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        mainTextView.setText("Button pressed!");
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+
 
 
     }

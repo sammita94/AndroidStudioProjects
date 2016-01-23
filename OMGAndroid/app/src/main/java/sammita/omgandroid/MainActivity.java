68efb5bc875f6780ed39766af339e08b06b5e40b
sammita94 <sammitachakravarti@yahoo.com>
2016-01-23 10:30:52 +0530

    -

--- OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
+++ OMGAndroid/app/src/main/java/sammita/omgandroid/MainActivity.java
@@ -1,23 +1,29 @@
 package sammita.omgandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity implements View.OnClickListener  {
+public class MainActivity extends Activity implements View.OnClickListener , AdapterView.OnItemClickListener   {
 
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
 
+    ShareActionProvider mShareActionProvider;
     ListView mainListView;
     ArrayAdapter<String> mArrayAdapter;
     ArrayList<String> mNameList = new ArrayList<String>();
@@ -48,15 +54,52 @@ public class MainActivity extends Activity implements View.OnClickListener  {
 
 // Set the ListView to use the ArrayAdapter
 
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
+
+        // 6. The text you'd like to share has changed,
+// and you need to update
+        setShareIntent();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
         return true;
     }
 
+    private void setShareIntent() {
+
+        if (mShareActionProvider != null) {
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
 
     @Override
     public void onClick(View v) {
@@ -68,5 +111,13 @@ public class MainActivity extends Activity implements View.OnClickListener  {
 
 
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position + ": " + mNameList.get(position));
     }
 }
